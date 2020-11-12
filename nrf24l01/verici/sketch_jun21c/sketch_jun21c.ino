492c84323e0913eeba6ea99be7124ab83a49f510
Melih Arık <meliharik@hotmail.com>
2020-11-12 12:43:40 +0300

    123

---
+++ nrf24l01/verici/sketch_jun21c/sketch_jun21c.ino
@@ -0,0 +1,19 @@
+#include <SPI.h>                      //the communication interface with the modem
+#include "RF24.h"                     //the library which helps us to control the radio modem
+
+int msg[1];
+
+RF24 radio(7,8);                     //5 and 10 are a digital pin numbers to which signals CE and CSN are connected.
+                                      
+const uint64_t pipe = 0xE8E8F0F0E1LL; //the address of the modem, that will receive data from Arduino.
+
+
+void setup(void){
+  radio.begin();                      //it activates the modem.
+  radio.openWritingPipe(pipe);        //sets the address of the receiver to which the program will send data.
+}
+
+void loop(void){
+  msg[0] =  map (analogRead(0), 0, 1023, 0, 179); 
+  radio.write(msg, 1);
+}
